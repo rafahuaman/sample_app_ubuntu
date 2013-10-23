4b39dc5f8250b7ac80906c4e61c66f64cd4997d5
Rafael-Ubuntu <leafar.huaman@gmail.com>
2013-10-23 00:44:50 -0400

    Complete 9.2

--- spec/support/session_utility.rb
+++ spec/support/session_utility.rb
@@ -8,7 +8,9 @@ module SessionUtility
 
 	RSpec::Matchers.define :have_signed_in_appearance do
 		match do |page| 
+			expect(page).to have_link('Settings',href: edit_user_path(user))
 			expect(page).to have_link('Sign out',href: signout_path)
+
 		end	
 	end
 
