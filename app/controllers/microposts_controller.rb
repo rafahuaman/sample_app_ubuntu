faa4247646ca014c9a87909d71c44b96bdfda1af
Rafael-Ubuntu <leafar.huaman@gmail.com>
2013-10-31 18:20:13 -0400

    finish listing 10.32

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,8 +2,21 @@ class MicropostsController < ApplicationController
 	before_action :signed_in_user
 
 	def create
+		@micropost = user.micropost.build(microposts_params)
+		if @microposts.save
+			flash[:success] = "Micropost created!"
+			redirect_to root_url
+		else
+			render 'static_pages/home'
+		end
 	end
 
 	def destroy
+	end
+
+	private
+
+	def microposts_params
+		params.require(:micropost).permit(:content)
 	end
 end
