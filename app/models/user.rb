1ef4d094c2ed22344c4531ac640a0d475bf78bab
Rafael-Ubuntu <leafar.huaman@gmail.com>
2013-11-30 21:11:00 -0500

    Finish 11.1.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
 	validates(:name, presence: true, length: { maximum: 50 })
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates(:email, presence: true, 
