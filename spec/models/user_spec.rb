2edd40f2e39f2f77d2cd1e9f5733dc7a9b3ffd05
Rafael-Ubuntu <leafar.huaman@gmail.com>
2013-12-08 00:29:36 -0500

    Ch 11 Exercises

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -202,5 +202,24 @@ describe User do
       it { should_not be_following(other_user) }
       its(:followed_users) { should_not include(other_user) }
     end
+
+    it "should destroy associated relationships" do
+      relationships = @user.relationships.to_a
+      @user.destroy
+      expect(relationships).not_to be_empty
+      relationships.each do |relationship|
+        expect(Relationship.where(id: relationship.id)).to be_empty
+      end
+    end
+
+    it "should destroy associated reverse relationships" do
+      other_user.follow!(@user)
+      relationships = @user.reverse_relationships.to_a
+      @user.destroy
+      expect(relationships).not_to be_empty
+      relationships.each do |relationship|
+        expect(Relationship.where(id: relationship.id)).to be_empty
+      end
+    end
   end
 end
